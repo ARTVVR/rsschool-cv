945da23c95ba9d8fff0a205904dc3eea1daf1f06
Artur Metelskiy <89966205+ARTVVR@users.noreply.github.com>
2021-09-20 23:12:13 +0300

    init: added fail

---
+++ menu.js
@@ -0,0 +1,8 @@
+const a = document.querySelector(".bt");
+const header = document.querySelector(".menu");
+
+function bt() {
+  a.classList.toggle("act");
+  header.classList.toggle("closed");
+}
+  
